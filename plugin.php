a7b7e32256df30b1ed23fb695c25d3a35b7e1614
Dave Lester <davelester@gmail.com>
2012-07-18 22:24:16 -0700

    Adds output context for pbcore, and will add link rel to individual item show pages.

--- plugin.php
+++ plugin.php
@@ -1,5 +1,8 @@
 <?php
 add_plugin_hook('install', 'install');
+add_filter('define_response_contexts', 'pbcoreOutputReponseContext');
+add_filter('define_action_contexts', 'pbcoreOutputActionContext');
+add_plugin_hook('public_theme_header', 'pbcoreThemeHeader');
 
 function install() {
 	$elementSetMetadata = array(
@@ -180,4 +183,35 @@ function pbcore_items_form_tabs($tabs, $item)
 {
 	unset($tabs['Dublin Core']);
 	return $tabs;
+}
+
+function pbcoreOutputReponseContext($context)
+{
+    $context['pbcore'] = array('suffix'  => 'pbcore', 
+                            'headers' => array('Content-Type' => 'text/xml'));
+
+    return $context;
+}
+
+function pbcoreOutputActionContext($context, $controller)
+{
+    if ($controller instanceof ItemsController) {
+        $context['show'][] = 'pbcore';
+    }
+
+    return $context;
+}
+
+function pbcoreThemeHeader()
+{
+	echo pbcoreOutput();
+}
+   
+function pbcoreOutput()
+{
+    $request = Zend_Controller_Front::getInstance()->getRequest();
+
+	if ($request->getControllerName() == 'items' && $request->getActionName() == 'show') {
+	    return '<link rel="alternate" type="application/rss+xml" href="'.item_uri().'?output=pbcore" id="pbcore"/>' . "\n";
+	}
 }
