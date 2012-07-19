7d7094dabcec5243dd6333b1ac0955730d419876
Dave Lester <davelester@gmail.com>
2012-07-18 22:19:05 -0700

    Replace references to Dublin Core with PBCore element set.

--- views/shared/items/show.pbcore.php
+++ views/shared/items/show.pbcore.php
@@ -1,64 +1,64 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'; ?>
-<pbcoreDescriptionDocument xmlns="http://www.pbcore.org/PBCore/PBCoreNamespace.html"
+<PBCoreDescriptionDocument xmlns="http://www.PBCore.org/PBCore/PBCoreNamespace.html"
               xmlns:mt="http://www.iana.org/assignments/media-types/"
               xmlns:la="http://www.loc.gov/standards/iso639-2/"
               xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
 		
 	<?php $item = get_current_item(); ?>
 
-	<pbcoreAssetDate dateType="Broadcast"><?php echo item('Item Type Metadata', 'Broadcast Date'); ?></pbcoreAssetDate>
-	<pbcoreIdentifier source="Internet Archive"><?php echo item('Dublin Core', 'Identifier'); ?></pbcoreIdentifier>
-	<pbcoreTitle><?php echo item('Dublin Core', 'Title'); ?></pbcoreTitle>
-	<pbcoreTitle titleType="Episode"><?php echo item('Dublin Core', 'Episode Title'); ?></pbcoreTitle>
-	<pbcoreTitle titleType="Series"><?php echo item('Dublin Core', 'Series Title'); ?></pbcoreTitle>
+	<PBCoreAssetDate dateType="Broadcast"><?php echo item('Item Type Metadata', 'Broadcast Date'); ?></PBCoreAssetDate>
+	<PBCoreIdentifier source="Internet Archive"><?php echo item('PBCore', 'Identifier'); ?></PBCoreIdentifier>
+	<PBCoreTitle><?php echo item('PBCore', 'Title'); ?></PBCoreTitle>
+	<PBCoreTitle titleType="Episode"><?php echo item('PBCore', 'Episode Title'); ?></PBCoreTitle>
+	<PBCoreTitle titleType="Series"><?php echo item('PBCore', 'Series Title'); ?></PBCoreTitle>
 
 	<?php $tags = get_tags(array('sort_field' => 'name'), null); 
 	foreach ($tags as $tag): ?>
-	<pbcoreSubject source="Free tags"><?php echo $tag; ?></pbcoreSubject>
+	<PBCoreSubject source="Free tags"><?php echo $tag; ?></PBCoreSubject>
 	<?php endforeach; ?>
 	
-	<pbcoreDescription><?php echo item('Dublin Core', 'Description'); ?></pbcoreDescription>
-	<pbcoreCoverage>
+	<PBCoreDescription><?php echo item('PBCore', 'Description'); ?></PBCoreDescription>
+	<PBCoreCoverage>
 		<coverage><?php echo "Not sure where to get geolocation info"; ?></coverage>
 		<coverageType>Spatial</coverageType>
-	</pbcoreCoverage>
-	<pbcoreCreator>
-		<creator><?php echo item('Dublin Core', 'Creator'); ?></creator>
+	</PBCoreCoverage>
+	<PBCoreCreator>
+		<creator><?php echo item('PBCore', 'Creator'); ?></creator>
 		<creatorRole>Creator</creatorRole>
-	</pbcoreCreator>   
+	</PBCoreCreator>   
 
-	<pbcoreContributor>
+	<PBCoreContributor>
 		<contributor><?php echo item('Item Type Metadata', 'Interviewee'); ?></contributor>
 		<contributorRole><?php echo "Interviewee"; ?></contributorRole>
-	</pbcoreContributor>
-	<pbcoreContributor>	
+	</PBCoreContributor>
+	<PBCoreContributor>	
 		<contributor><?php echo item('Item Type Metadata', 'Interviewer'); ?></contributor>
 		<contributorRole><?php echo "Interviewer"; ?></contributorRole>
-	</pbcoreContributor>
-	<pbcoreContributor>
+	</PBCoreContributor>
+	<PBCoreContributor>
 		<contributor><?php echo item('Item Type Metadata', 'Host'); ?></contributor>
 		<contributorRole><?php echo "Host"; ?></contributorRole>
-	</pbcoreContributor>
+	</PBCoreContributor>
 
-	<pbcoreRightsSummary>
+	<PBCoreRightsSummary>
 	    <rightsSummary></rightsSummary>
-	</pbcoreRightsSummary>
+	</PBCoreRightsSummary>
 
 	<!-- loop files here.. need to know how they're stored. -->
-	<pbcoreInstantiation>
+	<PBCoreInstantiation>
 		<instantiationIdentifier source="Filename"><?php echo "Not sure how to pull filename from Omeka DB"; ?></instantiationIdentifier>
-		<instantiationDigital><?php echo item('Dublin Core', 'Format'); ?></instantiationDigital>
-		<instantiationLocation><?php echo item('Dublin Core', 'Digital Location'); ?></instantiationLocation>
+		<instantiationDigital><?php echo item('PBCore', 'Format'); ?></instantiationDigital>
+		<instantiationLocation><?php echo item('PBCore', 'Digital Location'); ?></instantiationLocation>
 		<instantiationDuration><?php echo item('Item Type Metadata', 'Duration'); ?></instantiationLocation>
-	</pbcoreInstantiation>
+	</PBCoreInstantiation>
 	
-	<pbcoreInstantiation>
-		<instantiationPhysical><?php echo item('Dublin Core', 'Physical Format'); ?></instantiationPhysical>
-		<instantiationLocation><?php echo item('Dublin Core', 'Physical Location'); ?></instantiationLocation>
-	</pbcoreInstantiation>
+	<PBCoreInstantiation>
+		<instantiationPhysical><?php echo item('PBCore', 'Physical Format'); ?></instantiationPhysical>
+		<instantiationLocation><?php echo item('PBCore', 'Physical Location'); ?></instantiationLocation>
+	</PBCoreInstantiation>
 
-	<pbcoreAnnotation annotationType="Transcription"><?php echo item('Dublin Core', 'Transcription'); ?></pbcoreAnnotation>
-	<pbcoreAnnotation annotationType="Notes"><?php //echo item('Dublin Core', 'Notes'); ?></pbcoreAnnotation>
-	<pbcoreAnnotation annotationType="MusicUsed"><?php //echo item('Dublin Core', 'Music Used'); ?></pbcoreAnnotation>
+	<PBCoreAnnotation annotationType="Transcription"><?php echo item('PBCore', 'Transcription'); ?></PBCoreAnnotation>
+	<PBCoreAnnotation annotationType="Notes"><?php //echo item('PBCore', 'Notes'); ?></PBCoreAnnotation>
+	<PBCoreAnnotation annotationType="MusicUsed"><?php //echo item('PBCore', 'Music Used'); ?></PBCoreAnnotation>
 
-</pbcoreDescriptionDocument>
+</PBCoreDescriptionDocument>
